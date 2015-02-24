79436e483a19c3361cb74ee3b2ce35d78613fbf9
Haiko Schol <hs@haikoschol.com>
2015-02-24 11:28:18 +0000

    use a struct instead of a string in tests to enable Read() to return partial messages

--- gobot_test.go
+++ gobot_test.go
@@ -3,22 +3,42 @@ package main
 import "testing"
 import "math"
 
-type testMsg string
+type testMsg struct {
+	content   string
+	position  int
+	chunksize int
+}
 
 func (m testMsg) Read(p []byte) (n int, err error) {
 	err = nil
-	n = int(math.Min(float64(len(m)), float64(len(p))))
+	srcLen := float64(len(m.content) - m.position)
+	dstLen := float64(len(p))
 
-	for i := 0; i < n; i++ {
-		p[i] = m[i]
+	if m.chunksize > 0 {
+		n = int(math.Min(float64(m.chunksize), dstLen))
+	} else {
+		n = int(math.Min(srcLen, dstLen))
 	}
 
+	for i := m.position; i < n; i++ {
+		p[i] = m.content[i]
+	}
+
+	m.position += n
 	return
+}
+
+func makeMsg(s string, cs int) *testMsg {
+	return &testMsg{
+		content:   s + "\r\n",
+		position:  0,
+		chunksize: cs,
+	}
 }
 
 func TestReadMessage(t *testing.T) {
 	expected := "What's up?"
-	m := testMsg(expected + "\r\n")
+	m := makeMsg(expected, 0)
 
 	msg, err := ReadMessage(m)
 
